c413231272126e9599769e26bf96063fefd4bb91
bach.ngongoc97 <bach.ngongoc97@gmail.com>
2021-10-23 13:54:27 +0700

    Update code delete use soft delete

--- app/Category.php
+++ app/Category.php
@@ -3,9 +3,12 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Category extends Model
 {
+    // Sử dụng sort Delete
+    use SoftDeletes;
     //
     protected $fillable = [
         'name',
